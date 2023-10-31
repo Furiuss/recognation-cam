68b32426ec9d3d184dd1dc27455b248fc0f7c2f0
Andre Messias <andre.azevedo@inventsoftware.com.br>
2023-10-31 13:28:00 -0300

    tem que arrumar as paradas aqui e testar para ver se ta funcionando tudo nos conformes

--- detecao_facial.py
+++ detecao_facial.py
@@ -17,7 +17,7 @@ def detect_face_ssd(network, orig_frame, show_conf=True, conf_min=0.7):
     (h, w) = frame.shape[:2]
     # Pré-processamento da imagem para o formato aceitável pela rede neural:
     blob = cv2.dnn.blobFromImage(cv2.resize(frame, (300, 300)), 1.0, (300, 300), (104.0, 117.0, 123.0))
-    # é enviado a imagem para a camda de entrada da rede neural
+    # é enviado a imagem para a camada de entrada da rede neural
     network.setInput(blob)
 
     detections = network.forward()
@@ -26,13 +26,15 @@ def detect_face_ssd(network, orig_frame, show_conf=True, conf_min=0.7):
     for i in range(0, detections.shape[2]):
         confidence = detections[0, 0, i, 2]
         if confidence > conf_min:
-            bbox = detections[0, 0, i, 3:7] * np.array([w, h, w, h])                                                                   # Desenhar o a caixa delimitadora
+            # Desenhar o a caixa delimitadora
+            bbox = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
             (start_x, start_y, end_x, end_y) = bbox.astype("int")
 
             if (start_x<0 or start_y<0 or end_x > w or end_y > h):
                 continue
 
-            face_roi = orig_frame[start_y:end_y,start_x:end_x]                                                                        # Regiao de interesse da face
+            # Regiao de interesse da face
+            face_roi = orig_frame[start_y:end_y,start_x:end_x]
             face_roi = cv2.resize(face_roi, (90, 120))
             cv2.rectangle(frame, (start_x, start_y), (end_x, end_y), (0, 255, 0), 2)
             if show_conf:
